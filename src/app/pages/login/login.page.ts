2d3e67baf3e35a02b215c76d0d7b1566a5be2744
Victoorres <victoorres@icloud.com>
2020-05-01 21:18:09 -0300

    Validação Login Component

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  // variaveis
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private route: Router,
+    private builder: FormBuilder
+  ) { }
 
   ngOnInit() {
+
+    /* Reactive Form */
+    this.loginForm = this.builder.group(
+      {
+        email: ['', Validators.required],
+        senha: ['', Validators.required]
+      },
+      {}
+    );
+  }
+
+  onLogin() {
+    if (this.loginForm.get('email').value == 'victor@email.com' && this.loginForm.get('senha').value == '12345') {
+      this.route.navigateByUrl('/agenda');
+    } else {
+      alert('Email ou senha incorreto!')
+    }
   }
 
 }
