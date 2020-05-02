37f753841b414da30d83f58428598f75975d4ec8
AndreLss <andre_luis98@outlook.com>
2020-05-02 20:50:20 -0300

    Ajusta design da tela de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -8,33 +7,19 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  // variaveis
-  loginForm: FormGroup;
 
   constructor(
-    private route: Router,
-    private builder: FormBuilder
-  ) { }
+    private route: Router
+  ) {
+
+  }
 
   ngOnInit() {
 
-    /* Reactive Form */
-    this.loginForm = this.builder.group(
-      {
-        email: ['', Validators.required],
-        senha: ['', Validators.required]
-      },
-      {}
-    );
   }
 
-  onLogin() {
+  public login() {
     this.route.navigateByUrl('/agenda');
-/*     if (this.loginForm.get('email').value == 'victor@email.com' && this.loginForm.get('senha').value == '12345') {
-      this.route.navigateByUrl('/agenda');
-    } else {
-      alert('Email ou senha incorreto!')
-    } */
   }
 
 }
